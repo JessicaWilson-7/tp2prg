5ce197de09d5b8b0ef00900a1563d2adebb21daf
Jess <nomenjanaharimananajessica@gmail.com>
2024-04-05 16:47:02 +0300

    Etat initial

---
+++ src/main/java/com/company/tp2prg/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.company.tp2prg;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
