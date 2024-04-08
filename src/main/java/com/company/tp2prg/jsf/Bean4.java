7af46af0cdd627f82b10379213fc90501b854bb0
Jess <nomenjanaharimananajessica@gmail.com>
2024-04-08 15:27:57 +0300

    TP3 : Création des clients du template

--- src/main/java/com/company/tp2prg/jsf/Bean4.java
+++ src/main/java/com/company/tp2prg/jsf/Bean4.java
@@ -43,4 +43,8 @@ public class Bean4 {
             return "affichage_4?faces-redirect=true";
         }
     }
+    
+     public String afficher4() {
+        return "affichage__4?nb="+ nombre + "&amp;faces-redirect=true";
+    }
 }
